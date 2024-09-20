d567ca7cedcbaf69a380db11379e079b1afe3bf5
OscarLuquePorca <oscarluqueporca@gmail.com>
2024-09-20 12:57:23 +0200

    Kotlin Ejercicios 20/09

--- app/src/main/java/Ejercicio_6.kt
+++ app/src/main/java/Ejercicio_6.kt
@@ -1,9 +1,9 @@
 fun main(){
     println("Distancia del conductor en km: ")
 
-    var conductor = readLine()?.toInt()
+    val conductor = readLine()?.toInt()
     println("Disponibilidad: ")
-    var disponible = readLine().toBoolean()
+    val disponible = readLine().toBoolean()
 
     if (conductor != null) {
         if (conductor <= 0.5 && disponible == true){
